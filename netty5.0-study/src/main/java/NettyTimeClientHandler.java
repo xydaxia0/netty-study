fd32c81c16f775cb1ee4fb68bbc7fd6251794ff9
Yang Xu (徐洋3) <xuyang3@le.com>
2016-05-22 15:11:00 +0800

    粘包测试

--- netty5.0-study/src/main/java/NettyTimeClientHandler.java
+++ netty5.0-study/src/main/java/NettyTimeClientHandler.java
@@ -8,17 +8,22 @@ import io.netty.channel.ChannelHandlerContext;
  */
 public class NettyTimeClientHandler extends ChannelHandlerAdapter{
 
-    private final ByteBuf firstMsg;
+    private int counter;
+
+    private byte[] req;
 
     public NettyTimeClientHandler() {
-        byte[] req = "QUERY TIME ORDER".getBytes();
-        firstMsg = Unpooled.buffer(req.length);
-        firstMsg.writeBytes(req);
+        req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush(firstMsg);
+        ByteBuf message;
+        for (int i = 0; i < 100; i++) {
+            message = Unpooled.buffer();
+            message.writeBytes(req);
+            ctx.writeAndFlush(message);
+        }
     }
 
     @Override
@@ -27,7 +32,7 @@ public class NettyTimeClientHandler extends ChannelHandlerAdapter{
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
         String body = new String(req, "UTF-8");
-        System.out.println("Now is : " + body);
+        System.out.println("Now is : " + body + " ; the counter is : " + ++counter);
     }
 
     @Override
--- netty5.0-study/src/main/java/NettyTimeServerHandler.java
+++ netty5.0-study/src/main/java/NettyTimeServerHandler.java
@@ -11,16 +11,18 @@ import java.util.Date;
  */
 public class NettyTimeServerHandler extends ChannelHandlerAdapter {
 
+    private int counter;
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
-        String body = new String(req, "UTF-8");
-        System.out.println("The time server receive order : " + body);
+        String body = new String(req, "UTF-8").substring(0, req.length - System.getProperty("line.separator").length());
+        System.out.println("The time server receive order : " + body + " the counter is : " + ++counter);
         String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date().toString() : "BAD ORDER";
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-        ctx.write(resp);
+        ctx.writeAndFlush(resp);
     }
 
     @Override
